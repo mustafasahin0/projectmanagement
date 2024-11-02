50c4e2e4b1c4c740d763aa3959c4e0f6fc3c1f11
mustafasahin0 <sahinmustafaenes4@gmail.com>
2024-11-02 11:03:45 -0600

    back end

---
+++ server/src/controllers/taskController.ts
@@ -0,0 +1,88 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getTasks = async (req: Request, res: Response): Promise<void> => {
+  const { projectId } = req.query;
+  try {
+    const taks = await prisma.task.findMany({
+      where: {
+        projectId: Number(projectId),
+      },
+      include: {
+        author: true,
+        assignee: true,
+        comments: true,
+        attachments: true,
+      },
+    });
+    res.json(taks);
+  } catch (error: any) {
+    res.status(500).json({
+      message: `Error occured while retrievening taks: ${error.message}`,
+    });
+  }
+};
+
+export const createTask = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const {
+    title,
+    description,
+    status,
+    priority,
+    tags,
+    startDate,
+    dueDate,
+    points,
+    projectId,
+    authorUserId,
+    assignedUserId,
+  } = req.body;
+  try {
+    const newTask = await prisma.task.create({
+      data: {
+        title,
+        description,
+        status,
+        priority,
+        tags,
+        startDate,
+        dueDate,
+        points,
+        projectId,
+        authorUserId,
+        assignedUserId,
+      },
+    });
+    res.status(201).json(newTask);
+  } catch (error: any) {
+    res.status(500).json({
+      message: `Error occured while creating new task: ${error.message}`,
+    });
+  }
+};
+
+export const updateTaskStatus = async (req: Request, res: Response): Promise<void> => {
+    const { taskId } = req.params;
+    const { status } = req.body;
+    try {
+      const updatedTask = await prisma.task.update({
+        where: {
+          id: Number(taskId),
+        },
+        data: {
+            status: status,
+        }
+      });
+      res.json(updatedTask);
+    } catch (error: any) {
+      res.status(500).json({
+        message: `Error updating taks: ${error.message}`,
+      });
+    }
+  };
+  
