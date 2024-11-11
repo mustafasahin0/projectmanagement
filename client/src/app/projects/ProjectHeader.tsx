147bce122763063a5d9a1675bdc4589346ca1c9e
mustafasahin0 <sahinmustafaenes4@gmail.com>
2024-11-10 21:14:41 -0700

    added

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -5,10 +5,12 @@ import {
   Grid3x3,
   Grid3X3,
   List,
+  PlusSquare,
   Share2,
   Table,
 } from "lucide-react";
 import React, { use, useState } from "react";
+import ModalNewProject from "./ModalNewProject";
 
 type Props = {
   activeTab: string;
@@ -20,9 +22,23 @@ const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
 
   return (
     <div className="px-4 xl:px-6">
-      {/* MODAL NEW PROJECT */}
+      <ModalNewProject
+        isOpen={isModalNewProjectOpen}
+        onClose={() => setIsModalNewProjectOpen(false)}
+      />
+      {/* HEADER */}
       <div className="pb-6 pt-6 lg:pb-4 lg:pt-8">
-        <Header name="Product Design Development" />
+        <Header
+          name="Product Design Development"
+          buttonComponent={
+            <button
+              className="flex items-center rounded-md bg-blue-primary px-3 py-2 text-white hover:bg-blue-600"
+              onClick={() => setIsModalNewProjectOpen(true)}
+            >
+              <PlusSquare className="mr-2 h-5 w-5" /> New Boards
+            </button>
+          }
+        />
       </div>
       {/* TABS */}
       <div className="flex flex-wrap-reverse gap-2 border-y border-gray-200 bg-[8px] pt-2 dark:border-stroke-dark md:items-center">
